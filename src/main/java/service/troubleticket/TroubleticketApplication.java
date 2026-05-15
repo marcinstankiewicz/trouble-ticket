d2cb7ff6290c7de70fb702a306dc62b15d44e45d
Marcin Stankiewicz <marcin.stankiewicz@int.pl>
2026-05-15 13:44:37 +0200

    Init project

---
+++ src/main/java/service/troubleticket/TroubleticketApplication.java
@@ -0,0 +1,13 @@
+package service.troubleticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroubleticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroubleticketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/service/troubleticket/TroubleticketApplicationTests.java
@@ -0,0 +1,13 @@
+package service.troubleticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TroubleticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
